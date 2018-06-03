232c9fd629271199a64cbf50b14f4795814d8e6d
agimpel <agimpel@student.ethz.ch>
2018-06-03 20:48:45 +0200

    Add python scripts for testing

---
+++ test_scripts/report.py
@@ -0,0 +1,18 @@
+from urllib import parse, request
+import json
+import hashlib
+import hmac
+
+secret = b'1234'
+data = json.dumps({"rfid":"123456", "slot":"2"}).encode('utf8')
+headers = {'X-SIGNATURE': hmac.new(secret, data, hashlib.sha512).hexdigest(), 'Content-Type': 'application/json'}
+
+
+
+req = request.Request("http://localhost/endpoints/report.php", data = data, headers = headers)
+
+try:
+    resp = request.urlopen(req)
+    print(resp.read().decode('utf-8'))
+except Exception as e:
+    print(e)
