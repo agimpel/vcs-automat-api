bacbc394a3ae82b2c9d10b25f59bf5284b03fa1a
agimpel <agimpel@student.ethz.ch>
2018-08-16 21:22:06 +0200

    added new api hook for accessing general settings

---
+++ endpoints/info.php
@@ -0,0 +1,47 @@
+<?php
+
+define('ABSPATH', 1);
+
+// set up logging file and logging function
+require_once('../modules/logger.php');
+$logger = Logger::instance();
+$logger->setup('endpoint_info', 'DEBUG');
+
+// exit if the API is disabled
+require_once('../modules/sql_interface.php');
+$db = SQLhandler::instance();
+$enabled = $db->get_setting('api_active');
+if ($enabled !== '1') {
+	$logger->info('Dismissing, API is disabled.');
+	require_once('../modules/http_agent.php');
+	$response = new HTTP_Agent();
+	$response->send_response(503); //503: Service Unavailable
+	exit(); // Execution stops here
+}
+
+// catch HTTP request
+require_once('../modules/http_agent.php');
+$request = new HTTP_Agent();
+$request->catch_request();
+
+// if request is invalid based on signature, timestamp and nonce: dismiss
+if (!$request->valid_request()) {
+	$logger->info('Dismissing request based on invalidity.');
+	$response = new HTTP_Agent();
+	$response->send_response(403); //403: Forbidden
+	exit(); // Execution stops here
+}
+
+// return information about API
+$logger->info('Info request valid.');
+require_once('../modules/sql_interface.php');
+$db = SQLhandler::instance();
+$db_result = array();
+foreach (array('last_reset', 'next_reset', 'standard_credits', 'reset_interval') as $setting) {
+	$db_result[$setting] = $db->get_setting($setting);
+}
+$logger->debug('Query for settings succeeded.');
+$response = new HTTP_Agent();
+$response->send_response(200, $db_result); //200: OK
+exit(); // Execution stops here
+?>
